471210c90d6eeda64e10cc6735a84281ace72988
Fedor <fedykonovalov@gmail.com>
2019-12-05 20:19:44 +0300

    Updates

---
+++ precache-manifest.b0c6c385c938745c2138650986e6fa51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588f4f9406e252cacc498ec69f119a5e",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "f878f0281ffae5e6afae",
+    "url": "/ToDo/static/css/main.9786619f.chunk.css"
+  },
+  {
+    "revision": "258f6c440fd6eb4d764e",
+    "url": "/ToDo/static/js/2.d82e6745.chunk.js"
+  },
+  {
+    "revision": "f878f0281ffae5e6afae",
+    "url": "/ToDo/static/js/main.80ae8e3b.chunk.js"
+  },
+  {
+    "revision": "2172946647f367cda109",
+    "url": "/ToDo/static/js/runtime-main.b45e3869.js"
+  }
+]);
